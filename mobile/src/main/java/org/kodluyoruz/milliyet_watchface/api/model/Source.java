04c2c2d26c5e9a6b05da561dc7e971b7d8d8f564
Ahmet Rıfat Can Gür <cangur@androidedu.io>
2018-07-20 15:03:09 +0300

    Long Text Complication Added Complication Updating Event Added
    Signed-off-by: Ahmet Rıfat Can Gür <cangur@androidedu.io>

---
+++ mobile/src/main/java/org/kodluyoruz/milliyet_watchface/api/model/Source.java
@@ -0,0 +1,39 @@
+package org.kodluyoruz.milliyet_watchface.api.model;
+
+public class Source {
+    private String id;
+
+    private String name;
+
+    private String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassPojo [id = " + id + ", name = " + name + ", url = " + url + "]";
+    }
+}
+
